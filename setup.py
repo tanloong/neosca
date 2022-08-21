561038ab23ffbb1e99e8d174bb609a1b62ea0e05
Tan Long <2638000368@qq.com>
2022-08-21 15:21:57 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="nl2sca",
-    version="0.0.8",
+    version="0.0.9",
     author="TAN Long",
     author_email="tanloong@foxmail.com",
     url="https://github.com/tanloong/NeoL2SCA",
