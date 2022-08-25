f0e6c96390f1f6d16ac49f76c78d4e2e9ac93835
Tan Long <2638000368@qq.com>
2022-08-26 01:11:43 +0800

    manage version in __init__.py

--- setup.py
+++ setup.py
@@ -2,15 +2,24 @@ from distutils.core import setup
 
 with open('./README.md') as f:
     long_description = f.read()
+with open('./nl2sca/__init__.py') as f:
+    for line in f.readlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            version = line.split(delim)[1]
+            break
+    else:
+        print("Can't find version! Stop Here!")
+        exit(1)
 setup(
     name="nl2sca",
-    version="0.0.13",
+    version=version,
     author="TAN Long",
     author_email="tanloong@foxmail.com",
     url="https://github.com/tanloong/NeoL2SCA",
     packages=["nl2sca", "nl2sca.utils", "nl2sca.samples"],
     description="NeoL2SCA is a rewrite of Xiaofei Lu's L2 Syntactic Complexity Analyzer",
-    long_description=readme,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[],
     classifiers=[
