bd4d71a851c3ffad0d5b52e37b33c6b78ca5198d
Tan Long <tanloong@foxmail.com>
2023-01-02 22:40:18 +0800

    feat: add `--verbose` flag; improve performance.

--- neosca/parser.py
+++ neosca/parser.py
@@ -6,26 +6,39 @@ import sys
 
 
 class Parser:
-    parser_method = "edu.stanford.nlp.parser.lexparser.LexicalizedParser"
-    parser_model = "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz"
+    PARSER_METHOD = "edu.stanford.nlp.parser.lexparser.LexicalizedParser"
+    PARSER_MODEL = "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz"
+    NTHREADS = 2
+    MAX_MEMORY = "3g"
 
-    def __init__(self, dir_stanford_parser: str):
+    def __init__(
+        self,
+        dir_stanford_parser: str,
+        nthreads=NTHREADS,
+        max_memory=MAX_MEMORY,
+        verbose=False,
+    ) -> None:
         self.classpath = '"' + dir_stanford_parser + os.sep + "*" + '"'
+        self.max_memory = max_memory
+        self.nthreads = nthreads
+        self.verbose = verbose
 
     def parse(self, text: str, ofile_parsed: str) -> str:
         """Call Stanford Parser"""
-        print(f"\t[Parser] Parsing...")
         cmd = (
-            f'java -mx3g -cp {self.classpath} "{self.parser_method}"'
-            f" -outputFormat penn -nthreads 2 {self.parser_model} -"
+            f"java -mx{self.max_memory} -cp"
+            f' {self.classpath} "{self.PARSER_METHOD}" -outputFormat penn'
+            f" -nthreads {self.nthreads} {self.PARSER_MODEL} -"
         )
+        print(f"\t[Parser] Parsing with command '{cmd}'...")
         try:
             p = subprocess.run(
                 cmd,
                 input=text.encode("utf-8"),
                 shell=True,
                 check=True,
-                capture_output=True,
+                stdout=subprocess.PIPE,
+                stderr=None if self.verbose else subprocess.DEVNULL,
             )
         except subprocess.CalledProcessError as err_msg:
             print(err_msg)
