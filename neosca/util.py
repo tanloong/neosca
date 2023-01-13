a529b1a76e261b34eb3c3bd059060e10d98ef1b6
Tan Long <tanloong@foxmail.com>
2023-01-13 18:50:05 +0800

    fix: print a prompt message when {ofile_freq} is already in use by another process

--- neosca/util.py
+++ neosca/util.py
@@ -2,6 +2,13 @@
 # -*- coding=utf-8 -*-
 
 import sys
+from typing import Optional, Tuple
+
+
+# For all the procedures in SCAUI, return a tuple as the result
+# The first element bool indicates whether the procedure succeeds
+# The second element is the error message if it fails.
+SCAProcedureResult = Tuple[bool, Optional[str]]
 
 
 class _bcolors:
@@ -34,3 +41,19 @@ def color_print(color, s: str, **kwargs) -> None:
         print(bcolors.__getattribute__(color) + s + bcolors.ENDC, **kwargs)
     else:  # pragma: no cover
         print(s)
+
+
+def try_write(filename: str, content: Optional[str]) -> SCAProcedureResult:
+    try:
+        with open(filename, "w", encoding="utf-8") as f:
+            if content is not None:
+                f.write(content)
+            return True, None
+    except PermissionError:
+        return (
+            False,
+            f"PermissionError: can not write to {filename}, because it is already"
+            f" in use by another process.\n\n1. Ensure that {filename} is closed,"
+            " or \n2. Specify another output filename through the `-o` option,"
+            f" e.g. nsca input.txt -o {filename.replace('.csv', '-2.csv')}",
+        )
