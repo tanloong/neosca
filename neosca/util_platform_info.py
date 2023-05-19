e9710a0023f731738c914b938e8133f5a795d124
Tan Long <tanloong@foxmail.com>
2023-05-19 20:28:51 +0800

    fix: use root if username contains non-latin chars (close #19)
    https://answers.microsoft.com/en-us/windows/forum/all/non-latin-characters-in-username-should-be-avoided/25131f90-ebfe-4086-897f-d0f7522d87dc

--- neosca/util_platform_info.py
+++ neosca/util_platform_info.py
@@ -9,6 +9,10 @@ IS_DARWIN = sys.platform == "darwin"
 IS_LINUX = sys.platform == "linux"
 user_home = os.path.expanduser("~")
 if IS_WINDOWS:  # pragma: no cover
-    USER_SOFTWARE_DIR = os.getenv("AppData", os.path.join(user_home, "AppData", "Roaming"))
+    user_name = os.path.basename(user_home)
+    if user_name.isascii():
+        USER_SOFTWARE_DIR = os.getenv("AppData", os.path.join(user_home, "AppData", "Roaming"))
+    else: # if user_name contains non-latin chars
+        USER_SOFTWARE_DIR = os.path.splitdrive(user_home)[0] + os.path.sep # C:\
 else:
     USER_SOFTWARE_DIR = os.path.join(user_home, ".local", "share")
