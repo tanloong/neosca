434401a4b8a54f3da281f55c4dac47a8dadfb4f4
Tan Long <tanloong@foxmail.com>
2023-11-08 02:53:32 +0800

    revert: STANFORD_TREGEX_HOME is still needed by tregex_cli.py

--- neosca/scaenv.py
+++ neosca/scaenv.py
@@ -10,6 +10,9 @@ from typing import List, Optional
 from .scaplatform import IS_MAC, IS_LINUX, IS_WINDOWS
 from .scaprint import color_print
 
+# JAVA_HOME = "JAVA_HOME"
+# STANFORD_PARSER_HOME = "STANFORD_PARSER_HOME"
+STANFORD_TREGEX_HOME = "STANFORD_TREGEX_HOME"
 
 def _setenv_windows(env_var: str, paths: List[str], is_refresh: bool = False) -> None:
     import winreg  # Allows access to the windows registry
