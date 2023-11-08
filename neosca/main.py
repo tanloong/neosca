a0c7a2968e71df8728dc6c5adf9b23ae94892dfa
Tan Long <tanloong@foxmail.com>
2023-11-08 23:22:17 +0800

    revert: now files without extension won't be accepted

--- neosca/main.py
+++ neosca/main.py
@@ -7,14 +7,6 @@ import sys
 from typing import Callable, List, Optional
 
 from .about import __version__
-from .scaenv import (
-    JAVA_HOME,
-    STANFORD_PARSER_HOME,
-    STANFORD_TREGEX_HOME,
-    getenv,
-    search_java_home,
-    setenv,
-)
 from .scaio import SCAIO
 from .scaprint import color_print
 from .util import SCAProcedureResult
@@ -22,8 +14,7 @@ from .util import SCAProcedureResult
 
 class SCAUI:
     def __init__(self) -> None:
-        self.scaio = SCAIO()
-        self.supported_ifile_types = list(self.scaio.ext_read_map.keys())
+        self.supported_extensions = SCAIO.SUPPORTED_EXTENSIONS
         self.cwd = os.getcwd()
         self.args_parser: argparse.ArgumentParser = self.create_args_parser()
         self.options: argparse.Namespace = argparse.Namespace()
@@ -58,36 +49,6 @@ class SCAUI:
             ),
         )
         args_parser.add_argument(
-            "--max-length",
-            metavar="<max_length>",
-            dest="max_length",
-            type=int,
-            default=None,
-            help=(
-                "Set the longest sentence to parse (inclusively). Sentences longer than"
-                " <max_length> will be skipped, with a message printed to stderr. When this is"
-                " not specified, the program will try to analyze sentences of any lengths, but"
-                " may run out of memory trying to do so"
-                " (https://nlp.stanford.edu/software/parser-faq.html#k)."
-            ),
-        )
-        args_parser.add_argument(
-            "--newline-break",
-            dest="newline_break",
-            choices=["never", "always", "two"],
-            default="never",
-            help=(
-                "Whether to treat newlines as sentence breaks. This option has 3 legal values."
-                ' "never" means to ignore newlines for the purpose of sentence splitting, and is'
-                " appropriate for continuous text with hard line breaks when just the"
-                " non-whitespace characters should be used to determine sentence breaks."
-                ' "always" means to treat a newline as a sentence break, but there still may be'
-                ' more than one sentences per line. "two" means to take two or more consecutive'
-                " newlines as a sentence break, and is for text with hard line breaks and a"
-                ' blank line between paragraphs. The default is "never".'
-            ),
-        )
-        args_parser.add_argument(
             "--combine-files",
             "-c",
             metavar="<subfile>",
@@ -107,20 +68,13 @@ class SCAUI:
         args_parser.add_argument(
             "--ftype",
             dest="ifile_types",
-            choices=self.supported_ifile_types,
-            default=self.supported_ifile_types,
+            choices=self.supported_extensions,
+            default=self.supported_extensions,
             nargs="+",
             help=(
                 "Analyze files of the specified type(s). If not set, the program will process"
                 " files of all supported types."
             ),
-        )
-        args_parser.add_argument(
-            "--pretokenized",
-            dest="is_pretokenized",
-            action="store_true",
-            default=False,
-            help="Assume that the text has already been tokenized.",
         )
         args_parser.add_argument(
             "--output-file",
@@ -209,16 +163,6 @@ class SCAUI:
             help="Print detailed logging messages.",
         )
         args_parser.add_argument(
-            "--check-depends",
-            dest="check_depends",
-            action="store_true",
-            default=False,
-            help=(
-                "Check and install NeoSCA's dependencies: Java, Stanford Parser, and"
-                " Stanford Tregex."
-            ),
-        )
-        args_parser.add_argument(
             "--yes",
             dest="is_assume_yes",
             action="store_true",
@@ -254,31 +198,27 @@ class SCAUI:
     Analyze files ranging from sample101.txt to sample200.txt.
 10. nsca sample10[1-9].txt sample1[1-9][0-9].txt sample200.txt --expand-wildcards
     Expand the specified wildcards and exit.
-11. nsca sample1.txt --max-length 100
-    Only analyze sentences with lengths shorter than or equal to 100.
-12. nsca sample1.txt --newline-break always
-    Consider newlines as sentence breaks.
-13. nsca --select VP T DC/C -- sample1.txt
+11. nsca --select VP T DC/C -- sample1.txt
     Select a subset of measures to analyze. Use -- to separate input
     filenames from the selected measures, or otherwise the program will take
     "sample1.txt" as a measure and then raise an error. Arguments other than
     input filenames should be specified at the left side of --.
-14. nsca -c sample1-sub1.txt sample1-sub2.txt
+12. nsca -c sample1-sub1.txt sample1-sub2.txt
     Add up frequencies of the 9 syntactic structures of the subfiles and compute
     values of the 14 syntactic complexity indices for the imaginary parent file.
-15. nsca -c samples/
+13. nsca -c samples/
     Combine all the input files within the "samples/" directory
-16. nsca -c sample1-sub*.txt
+14. nsca -c sample1-sub*.txt
     Wildcards are supported for -c.
-17. nsca -c sample1-sub*.txt -c sample2-sub*.txt
+15. nsca -c sample1-sub*.txt -c sample2-sub*.txt
     Use multiple -c to combine different lists of subfiles respectively.
-18. nsca -c sample1-sub*.txt -c sample2-sub*.txt -- sample[3-9].txt
+16. nsca -c sample1-sub*.txt -c sample2-sub*.txt -- sample[3-9].txt
     Use -- to separate input filenames from names of the subfiles.
-19. nsca sample1.txt --no-query
+17. nsca sample1.txt --no-query
     Parse the input files, save the parsed trees and exit.
-20. nsca sample1.parsed --no-parse
+18. nsca sample1.parsed --no-parse
     Assume input as parse trees. Skip the parsing step and proceed directly to querying.
-21. nsca --config nsca.json sample1.txt
+19. nsca --config nsca.json sample1.txt
     Use nsca.json where you can defined your own syntactic structures to search or calculate.
 
 Contact:
@@ -318,7 +258,7 @@ Contact:
             logging.info(f"Command-line text: {options.text}")
             self.verified_ifiles = None
         else:
-            self.verified_ifiles = self.scaio.get_verified_ifile_list(ifile_list)
+            self.verified_ifiles = SCAIO().get_verified_ifile_list(ifile_list)
 
         if options.subfiles_list is None:
             self.verified_subfiles_list: List[list] = []
@@ -362,12 +302,6 @@ Contact:
         else:
             options.ofile_freq = "result." + options.oformat_freq
 
-        is_max_length_given_and_lt_zero = (
-            options.max_length is not None and options.max_length < 0
-        )
-        if is_max_length_given_and_lt_zero or options.max_length == 0:
-            return False, 'The value of "--max-length" should be greater than 0.'
-
         if options.selected_measures is not None:
             # Drop duplicates while retain order. Starting from Python 3.7, the
             # built-in dictionary is guaranteed to maintain the insertion order
@@ -391,124 +325,17 @@ Contact:
         self.init_kwargs = {
             "ofile_freq": options.ofile_freq,
             "oformat_freq": options.oformat_freq,
-            "stanford_parser_home": "",
-            "stanford_tregex_home": "",
             "odir_matched": self.odir_matched,
-            "newline_break": options.newline_break,
-            "max_length": options.max_length,
             "selected_measures": options.selected_measures,
             "is_reserve_parsed": options.is_reserve_parsed,
             "is_reserve_matched": options.is_reserve_matched,
             "is_stdout": options.is_stdout,
             "is_skip_querying": options.is_skip_querying,
             "is_skip_parsing": options.is_skip_parsing,
-            "is_pretokenized": options.is_pretokenized,
             "config": user_config,
         }
         self.options = options
         return True, None
-
-    def check_java(self) -> SCAProcedureResult:
-        java_home = getenv(JAVA_HOME)
-        if java_home is None:
-            logging.debug("JAVA_HOME either does not exist or is not a valid dirname.")
-            java_home = search_java_home()
-            if java_home is None:
-                from .depends_installer import DependsInstaller
-                from .depends_installer import JAVA
-
-                installer = DependsInstaller()
-                sucess, err_msg = installer.install(
-                    JAVA, is_assume_yes=self.options.is_assume_yes
-                )
-                if not sucess:
-                    return sucess, err_msg
-                else:
-                    java_home = err_msg
-            java_bin = os_path.join(java_home, "bin")  # type:ignore
-            path_orig = os.getenv("PATH", "")
-            setenv(
-                "JAVA_HOME",
-                [java_home],  # type:ignore
-                is_override=True,
-                is_quiet=self.options.is_quiet,
-            )
-            setenv(
-                "PATH", [java_bin], is_override=False, is_quiet=self.options.is_quiet
-            )  # type:ignore
-            os.environ["JAVA_HOME"] = java_home  # type:ignore
-            os.environ["PATH"] = java_bin + os.pathsep + path_orig  # type:ignore
-        elif not self.options.is_quiet:
-            color_print("OKGREEN", "ok", prefix="Java has already been installed. ")
-        return True, None
-
-    def check_stanford_parser(self) -> SCAProcedureResult:
-        stanford_parser_home = getenv(STANFORD_PARSER_HOME)
-        if stanford_parser_home is None:
-            from .depends_installer import DependsInstaller
-            from .depends_installer import STANFORD_PARSER
-
-            installer = DependsInstaller()
-            sucess, err_msg = installer.install(
-                STANFORD_PARSER, is_assume_yes=self.options.is_assume_yes
-            )
-            if not sucess:
-                return sucess, err_msg
-            else:
-                stanford_parser_home = err_msg
-                assert stanford_parser_home is not None
-                setenv(
-                    STANFORD_PARSER_HOME,
-                    [stanford_parser_home],
-                    is_override=True,
-                    is_quiet=self.options.is_quiet,
-                )
-        elif not self.options.is_quiet:
-            color_print("OKGREEN", "ok", prefix="Stanford Parser has already been installed. ")
-
-        self.init_kwargs.update({"stanford_parser_home": stanford_parser_home})
-        return True, None
-
-    def check_stanford_tregex(self) -> SCAProcedureResult:
-        stanford_tregex_home = getenv(STANFORD_TREGEX_HOME)
-        if stanford_tregex_home is None:
-            from .depends_installer import DependsInstaller
-            from .depends_installer import STANFORD_TREGEX
-
-            installer = DependsInstaller()
-            sucess, err_msg = installer.install(
-                STANFORD_TREGEX, is_assume_yes=self.options.is_assume_yes
-            )
-            if not sucess:
-                return sucess, err_msg
-            else:
-                stanford_tregex_home = err_msg
-                assert stanford_tregex_home is not None
-                setenv(
-                    STANFORD_TREGEX_HOME,
-                    [stanford_tregex_home],
-                    is_override=True,
-                    is_quiet=self.options.is_quiet,
-                )
-        elif not self.options.is_quiet:
-            color_print("OKGREEN", "ok", prefix="Stanford Tregex has already been installed. ")
-        self.init_kwargs.update({"stanford_tregex_home": stanford_tregex_home})
-        return True, None
-
-    def check_depends(self) -> SCAProcedureResult:
-        success_java, err_msg_java = self.check_java()
-        success_parser, err_msg_parser = self.check_stanford_parser()
-        success_tregex, err_msg_tregex = self.check_stanford_tregex()
-
-        sucesses = (success_java, success_parser, success_tregex)
-        err_msges = (err_msg_java, err_msg_parser, err_msg_tregex)
-        if all(sucesses):
-            return True, None
-        else:
-            err_msg = "\n\n".join(
-                map(lambda p: p[1] if not p[0] else "", zip(sucesses, err_msges))  # type:ignore
-            )
-            return False, err_msg.strip()
 
     def check_python(self) -> SCAProcedureResult:
         v_info = sys.version_info
@@ -567,9 +394,6 @@ Contact:
             sucess, err_msg = self.check_python()
             if not sucess:
                 return sucess, err_msg
-            sucess, err_msg = self.check_depends()
-            if not sucess:
-                return sucess, err_msg
             if not self.options.is_stdout:
                 sucess, err_msg = SCAIO.is_writable(self.options.ofile_freq)
                 if not sucess:
@@ -604,8 +428,6 @@ Contact:
             return self.list_fields()
         elif self.options.expand_wildcards:
             return self.expand_wildcards()
-        elif self.options.check_depends:
-            return self.check_depends()
         elif self.options.text is not None:
             return self.run_on_text()  # type: ignore
         elif self.verified_ifiles or self.verified_subfiles_list:
